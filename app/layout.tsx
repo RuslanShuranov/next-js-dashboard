8e0803c53a6474d563072d25bdc5faa081ba65d5
Ruslan Shuranov <ruslanshuranov@gmail.com>
2025-03-21 00:00:34 +0100

    update layout structure and add navigation links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import '@/app/ui/global.css'
-import { clsx } from 'clsx'
-import { inter } from '@/app/ui/fonts'
+import React from 'react';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  )
-}
+  );
+};
+
+export default RootLayout;
