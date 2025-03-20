6de3da5223a4bf9f561b21d2c499c29d8492aa55
Ruslan Shuranov <ruslanshuranov@gmail.com>
2025-03-20 23:34:00 +0100

    add fonts, configure prettier, and update various files for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import React from 'react'
+import '@/app/ui/global.css'
+import { clsx } from 'clsx'
+import { inter } from '@/app/ui/fonts'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
-  );
+  )
 }
