34153cafc2ac609edb4f6736fcef28654470d080
Ruslan Shuranov <ruslanshuranov@gmail.com>
2025-03-21 13:16:42 +0100

    refactor: update dashboard components to use async data fetching and add loading states

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,5 +1,6 @@
 import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -8,24 +9,23 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+const CardWrapper = async () => {
+  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } =
+    await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" />
     </>
   );
-}
+};
 
-export function Card({
+export default CardWrapper;
+
+export const Card = ({
   title,
   value,
   type,
@@ -33,7 +33,7 @@ export function Card({
   title: string;
   value: number | string;
   type: 'invoices' | 'customers' | 'pending' | 'collected';
-}) {
+}) => {
   const Icon = iconMap[type];
 
   return (
@@ -49,4 +49,4 @@ export function Card({
       </p>
     </div>
   );
-}
+};
